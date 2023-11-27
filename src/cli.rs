f57483d918ede2157c1ce2fef789ce135328d2e7
whatsacomputertho <ebalcik71@gmail.com>
2023-11-27 10:59:59 -0500

    Initialize elevate-cli using elevator-optimization source

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+//Import library modules
+use clap::{Parser};
+
+/** ElevatorCli struct schema
+ *
+ * The ElevatorCli struct is used to store the command line
+ * arguments passed into the application/
+ */
+#[derive(Parser)]
+#[command(name="Elevator Optimization")]
+#[command(author="whatsacomputertho")]
+#[command(version="0.1.0")]
+#[command(
+    about="Simulate an elevator, measure wait time and energy usage",
+    long_about="The Elevator Optimization CLI implements elevator simulation logic. \
+                It models people arriving and leaving to and from the building, and \
+                measures the elevator's energy usage, as well as average wait time \
+                throughout the building.  The objective is to minimize with respect \
+                to these measurements under various conditions."
+)]
+pub struct ElevatorCli {
+    /// Number of floors in the building
+    #[arg(long="floors")]
+    pub floors: Option<usize>,
+
+    /// Number of elevators in the building
+    #[arg(long="elevators")]
+    pub elevators: Option<usize>,
+
+    /// Expected number of arrivals per time step
+    #[arg(long="expected-arrivals")]
+    pub arrivals: Option<f64>
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+mod cli;
+
+//Import source modules
+use crate::cli::ElevatorCli;
+
+//Import libraries
+use elevate_lib::building::Building;
+use elevate_lib::elevators::Elevators;
+use elevate_lib::floors::Floors;
+use elevate_lib::controller::{ElevatorController, RandomController};
+use std::{thread, time};
+use std::io::{Write, stdout};
+use crossterm::{terminal, cursor, QueueableCommand};
+use clap::Parser;
+
+//Main function
+fn main() {
+    //Parse the command line args
+    let cli_args = ElevatorCli::parse();
+    let num_floors: usize = match cli_args.floors {
+        Some(x) => x as usize,
+        None => 4_usize
+    };
+    let num_elevators: usize = match cli_args.elevators {
+        Some(x) => x as usize,
+        None => 2_usize
+    };
+    let expected_arrivals: f64 = match cli_args.arrivals {
+        Some(x) => x as f64,
+        None => 0.2_f64
+    };
+
+    //Initialize the building
+    let building = Building::from(
+        num_floors,
+        num_elevators,
+        expected_arrivals,
+        5.0_f64, //Base energy spent moving elevator up
+        2.5_f64, //Base energy spent moving elevator down
+        0.5_f64  //Coefficient for energy spent by moving N people
+    );
+
+    //Initialize the controller
+    let controller_rng = rand::thread_rng();
+    let mut controller = RandomController::from(
+        building, controller_rng
+    );
+
+    //Initialize the RNG and stdout
+    let mut rng = rand::thread_rng();
+    let mut stdout = stdout();
+    
+    //Loop until the numer of time steps are complete
+    let time_steps: i32 = 1000_i32;
+    for i in 0..time_steps {
+        //Generate people arriving and leaving
+        controller.building.gen_people_arriving(&mut rng);
+        controller.building.gen_people_leaving(&mut rng);
+
+        //Move people on and off the elevators and out of the building
+        controller.building.flush_first_floor();
+        controller.building.exchange_people_on_elevator();
+
+        //Update the elevators
+        controller.update_elevators();
+
+        //Increment the wait times, update average energy, update dest probabilities
+        let energy_spent: f64 = controller.building.elevators.get_energy_spent();
+        controller.building.increment_wait_times();
+        controller.building.update_average_energy(i, energy_spent);
+        controller.building.update_dest_probabilities();
+
+        //Print the rendered building status
+        let building_str: String = String::from(controller.building.to_string());
+        let building_str_len = building_str.matches("\n").count() as u16;
+        let _ = stdout.write_all(building_str.as_bytes());
+        stdout.flush().unwrap();
+
+        //Sleep for one second in between time steps
+        let one_sec = time::Duration::from_millis(100_u64);
+        thread::sleep(one_sec);
+
+        //Reset the cursor and clear the previous console output
+        if i < time_steps - 1 {
+            stdout.queue(cursor::MoveUp(building_str_len)).unwrap();
+            stdout.queue(cursor::MoveToColumn(0)).unwrap();
+            stdout.queue(terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
+        }
+    }
+}
